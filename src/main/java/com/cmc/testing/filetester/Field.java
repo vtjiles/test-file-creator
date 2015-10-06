cb1ace1d548c2139d34974e4cf7b43c0ef0885e6
Jay Hogan <jhogan@singlestoneconsulting.com>
2015-10-06 06:58:08 -0400

    Remove equals method from Field

--- src/main/java/com/cmc/testing/filetester/Field.java
+++ src/main/java/com/cmc/testing/filetester/Field.java
@@ -20,16 +20,4 @@ public class Field {
     public int getLength() {
         return length;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Field field = (Field) o;
-
-        if (name != null ? !name.equals(field.name) : field.name != null) return false;
-
-        return true;
-    }
 }
